494a18e2f03f02fcf96542f7600aa546f2b3ca18
kr4k3nb1t3 <95190880+kr4k3nb1t3@users.noreply.github.com>
2022-03-07 00:32:17 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-sudo apt-get update && apt-get install parrot-zsh parrot-privacy parrot-devel parrot-devel-tools parrot-devel-extra parrot-devel-node parrot-devel-dotnet parrot-devel-nim parrot-devel-golang vscodium vscode code parrot-tools-info parrot-tools-vuln parrot-tools-web parrot-tools-pwn parrot-tools-maintain parrot-tools-postexploit parrot-tools-password parrot-tools-wireless parrot-tools-sniff parrot-tools-forensic parrot-tools-automotive parrot-tools-reversing parrot-tools-report parrot-tools-full parrot-tools-cloud parrot-tools-common -y
+rm /etc/apt/trusted.gpg 
+wget https://deb.parrot.sh/parrot/pool/main/p/parrot-archive-keyring/parrot-archive-keyring_2021.7%2bparrot1_all.deb
+dpkg -i parrot-archive-keyring_2021.7+parrot1_all.deb
+sudo apt-get update && apt-get install parrot-meta-crypto parrot-meta-privacy parrot-meta-devel parrot-meta-devel-tools parrot-meta-devel-extra parrot-meta-devel-python parrot-meta-devel-node parrot-meta-devel-dotnet parrot-meta-devel-nim parrot-meta-devel-golang vscodium vscode code parrot-tools-infogathering parrot-tools-vuln parrot-tools-web parrot-tools-pwn parrot-tools-maintain parrot-tools-postexploit parrot-tools-password parrot-tools-wireless parrot-tools-sniff parrot-tools-forensic parrot-tools-automotive parrot-tools-reversing parrot-tools-reporting parrot-tools-full parrot-tools-cloud parrot-tools-common -y
